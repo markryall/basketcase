42914b6e0385be7fa446db86f2dbe682159068e7
Mark Ryall <mark.ryall@gmail.com>
2009-08-23 22:50:29 +1000

    added a script for faking cleartool and a couple of additional specs

--- spec/basketcase_spec.rb
+++ spec/basketcase_spec.rb
@@ -8,7 +8,7 @@ describe Basketcase do
     @basketcase = Basketcase.new
     @basketcase.stub!(:exit)
     $stderr.stub!(:puts, :sync)
-    $stdout.stub!(:puts)
+    $stdout.stub!(:puts, :sync)
   end
   
   it 'should sync stderr' do
@@ -19,5 +19,25 @@ describe Basketcase do
   it 'should sync stdout' do
     $stdout.should_receive(:sync=).with(true)
     @basketcase.do('ls')
+  end
+  
+  it 'should determine test mode from command line option --test' do
+    @basketcase.do('--test','ls')
+    @basketcase.instance_variable_get('@test_mode').should == true
+  end
+  
+  it 'should determine test mode from command line option -t' do
+    @basketcase.do('-t','ls')
+    @basketcase.instance_variable_get('@test_mode').should == true
+  end
+  
+  it 'should determine debug mode from command line option --test' do
+    @basketcase.do('--debug','ls')
+    @basketcase.instance_variable_get('@debug_mode').should == true
+  end
+  
+  it 'should determine debug mode from command line option -te' do
+    @basketcase.do('-d','ls')
+    @basketcase.instance_variable_get('@debug_mode').should == true
   end
 end
