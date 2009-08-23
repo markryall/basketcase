19967e1929fb7b896e58d92b620e5a80a21b2143
Duana Stanley <dstanley@Duana-Stanleys-TW-Oz-MacBook-Pro.local>
2009-08-23 23:29:22 +1000

    made huge breakthrough

--- spec/auto_sync_spec.rb
+++ spec/auto_sync_spec.rb
@@ -1,5 +1,18 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Autosync do
+describe 'Autosync' do
+  
+  it 'should be able to handle lots of files that need to be added' do 
+    
+    File.open(File.dirname(__FILE__)+'/ct.output', 'w') do |io|
+      30.times do |i|
+        io.puts "file #{i}"
+      end
+
+    end
+    
+    puts `#{File.dirname(__FILE__)}/cleartool`
+    Basketcase.new.do('auto-sync')
+  end
   
 end
