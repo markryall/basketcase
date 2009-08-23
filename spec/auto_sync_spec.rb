1fee9a1186cf2911ea23a82f96d1e1ccf908fcce
Mark Ryall <mark.ryall@gmail.com>
2009-08-24 00:04:15 +1000

    added a mechanism for faking a sequence of cleartool interactions

--- spec/auto_sync_spec.rb
+++ spec/auto_sync_spec.rb
@@ -4,15 +4,22 @@ describe 'Autosync' do
   
   it 'should be able to handle lots of files that need to be added' do 
     
+    ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__))}:#{ENV['PATH']}"
+    
+    File.delete(File.dirname(__FILE__)+'/../ct.log')
+        
     File.open(File.dirname(__FILE__)+'/ct.output', 'w') do |io|
-      30.times do |i|
+      10.times do |i|
         io.puts "file #{i}"
       end
-
+      io.puts '++++'
+      10.times do |i|
+        io.puts "directory #{i}"
+      end
+      io.puts '++++'
+      io.puts 'foo'
     end
     
-    puts `#{File.dirname(__FILE__)}/cleartool`
-    Basketcase.new.do('auto-sync')
+    Basketcase.new.do('auto-sync','-n')
   end
-  
 end
