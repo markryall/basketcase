0bc7d6b1007f5084d6b7911d7e3542fca885bd97
Mark Ryall <mark.ryall@gmail.com>
2009-08-24 23:30:13 +1000

    replaced heredocs with string literals to avoid problems interpreting paths

--- spec/auto_sync_spec.rb
+++ spec/auto_sync_spec.rb
@@ -11,32 +11,31 @@ describe 'Autosync' do
     File.delete(logfile) if File.exists?(logfile) 
         
     File.open(File.dirname(__FILE__)+'/ct.output', 'w') do |io|
-      io.puts <<EOF
+      io.puts %q{
 deleteddir1@@\main\2 [loaded but missing]                Rule: \main\LATEST
 deletedfile1.txt@@\main\1 [loaded but missing]           Rule: \main\LATEST
 lost+found@@\main\0                                      Rule: \main\LATEST
-lsrecurse
 newdir1
 newfile1.txt
 newfile2.txt
 updateddir1@@\main\2                                     Rule: \main\LATEST
 updatedfiled1.txt@@\main\1 [hijacked]                    Rule: \main\LATEST
-.\deleteddir1\deletedfile1.txt@@\main\1 [loaded but missing]             Rule: \main\LATEST
-.\newdir1\newfile1.txt
-.\updateddir1\deletedfile1.txt@@\main\1 [loaded but missing]             Rule: \main\LATEST
-.\updateddir1\newfile1.txt
-.\updateddir1\updatedfiled1.txt@@\main\1 [hijacked]      Rule: \main\LATEST
-EOF
+deleteddir1\deletedfile1.txt@@\main\1 [loaded but missing]             Rule: \main\LATEST
+newdir1\newfile1.txt
+updateddir1\deletedfile1.txt@@\main\1 [loaded but missing]             Rule: \main\LATEST
+updateddir1\newfile1.txt
+updateddir1\updatedfiled1.txt@@\main\1 [hijacked]      Rule: \main\LATEST
+}
       io.puts '++++'
-      io.puts <<EOF
+      io.puts %q{
 .@@\main\12                                              Rule: \main\LATEST
-EOF
+}
       io.puts '++++'
-      io.puts <<EOF
+      io.puts %q{
 cleartool: Error: Can't modify directory "." because it is not checked out.
 cleartool: Error: Can't modify directory "." because it is not checked out.
 cleartool: Error: Can't modify directory "." because it is not checked out.
-EOF
+}
     end
     
     Basketcase.new.do('auto-sync','-n')
