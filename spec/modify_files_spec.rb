b24e027d73a88373f244fd1b518dcb9d9be13bd7
Duana Stanley <dstanley@Duana-Stanleys-TW-Oz-MacBook-Pro.local>
2009-09-07 21:15:18 +1000

    added tests for update and removing files

--- spec/modify_files_spec.rb
+++ spec/modify_files_spec.rb
@@ -1,5 +1,5 @@
 
-
+require 'pathname'
 require File.dirname(__FILE__)+'/spec_helper'
 load 'cleartool'
 
@@ -28,8 +28,52 @@ describe  'modify files' do
       Basketcase.new.do('update', '.')    
       File.exists?(newfile).should == true
     end  
+    
+    @@created_file = newfile
   end
   
+  it 'should update a file in clearcase' do
+    
+    new_content = 'i will love you as long as this file exists'
+    
+    Dir.chdir(WORKING_VIEW) do
+      
+      file = Pathname(@@created_file)
+      file.chmod(file.stat.mode | 0600) unless file.writable?
+      
+      File.open(@@created_file, 'a') { |io| io.puts new_content}
+      Basketcase.new.do('co', '-h', @@created_file)
+      Basketcase.new.do('ci', '-m', 'updating test file', @@created_file)
+    end
+    
+    Dir.chdir(CONTROL_VIEW) do
+      Basketcase.new.do('update', '.')
+      
+      file_contents = File.read(@@created_file)
+      file_contents.include?(new_content)
+      
+    end  
+  
+  end
+  
+  it 'should delete a file from clearcase' do
+    
+    Dir.chdir(WORKING_VIEW) do
+       File.exists?(@@created_file).should == true
+       
+       Basketcase.new.do('remove', @@created_file)
+       Basketcase.new.do('ci', '-m', 'removing test file', @@created_file)
+       
+     end
+     
+    Dir.chdir(CONTROL_VIEW) do
+      Basketcase.new.do('update', '.')    
+      File.exists?(@@created_file).should == false
+    end  
+    
+  end
+  
+
   
   
   def create_file(filename, content)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,8 @@ ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__))}:#{ENV['PATH']}"
 
 ROOT = File.expand_path(File.dirname(__FILE__)+'/..')
 
-WORKING_VIEW = "#{ROOT}/tmp/views/working-view"
-CONTROL_VIEW = "#{ROOT}/tmp/views/control-view"
+WORKING_VIEW = ENV['BC_WORKING_VIEW']
+CONTROL_VIEW = ENV['BC_CONTROL_VIEW']
+
+WORKING_VIEW ||= "#{ROOT}/tmp/views/working-view"
+CONTROL_VIEW ||= "#{ROOT}/tmp/views/control-view"
